96aa3a47a3ddadafe3e19bbfb10325b146ff1a2c
yujunjie <951222368>
2020-08-25 15:49:31 +0800

    修改auth服务、添加到注册中心

--- auth-server/src/main/java/com/example/authserver/AuthServerApplication.java
+++ auth-server/src/main/java/com/example/authserver/AuthServerApplication.java
@@ -2,7 +2,9 @@ package com.example.authserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication(scanBasePackages = {"com.junjie.common","org.junjie.security.*", "com.example.authserver"})
 public class AuthServerApplication {
 
